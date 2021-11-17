0ff3a2d238fcab09eeb6700d2c48075671dc272d
demidraksin <greatraksin@icloud.com>
2021-11-17 13:27:49 +0300

    Adding conditions

---
+++ 10_lesson_1711/03_условия.js
@@ -0,0 +1,12 @@
+if (25 ** 3 > 438574) { // если
+    console.log('Оно больше!');
+} else if (25 ** 3 > 500) { // иначе если
+    console.log('Больше 500')
+} else { // иначе
+    console.log('Оно меньше!');
+}
+
+let age = 19;
+let allowAccess = (age > 18) ? true : false
+console.log(allowAccess)
+
