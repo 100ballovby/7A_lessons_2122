c812469f9d26b2c170793bbb14e3a88d814a99af
demidraksin <greatraksin@icloud.com>
2021-11-24 11:57:20 +0300

    Adding conditions

---
+++ 11_lesson_2411/hw/script.js
@@ -0,0 +1,15 @@
+let num = Number(prompt('Введите число: ', ''))
+// Сразу преобразовываю тип данных в числовой
+
+if (num > 0) {
+    alert(1);
+} else if (num < 0) {
+    alert(-1);
+} else if (num === 0) {
+    alert(0);
+} else {
+    alert('Таких чисел не бывает!')
+}
+
+
+
