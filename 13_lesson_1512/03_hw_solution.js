6d275526aa9a6ceeb64bb1b53b3faefe5d40b9ab
demidraksin <greatraksin@icloud.com>
2022-01-12 11:44:56 +0300

    Adding logic operations

---
+++ 13_lesson_1512/03_hw_solution.js
@@ -0,0 +1,8 @@
+let optimalTime = 30;
+let walkTime = 12;
+
+if (walkTime < optimalTime) {
+    console.log('Осталось гулять: ' + (optimalTime - walkTime) + ' минут')
+} else {
+    console.log('Вы погуляли достаточно! Осталось гулять: 0 минут.')
+}
---
+++ поал.js
@@ -0,0 +1,2 @@
+console.log(Math.floor(56 / 10))
+console.log(56 % 10)
